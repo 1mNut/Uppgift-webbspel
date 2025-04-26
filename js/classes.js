c6c0b97ab974c46c9af1132e1e2d1ff9a74b17a1
NanabuhSnatcher9-11Gobbler0fBananaBUH <alexander.malm@skola.taby.se>
2025-04-26 17:25:13 +0200

    ItemClasses klara.

--- js/classes.js
+++ js/classes.js
@@ -1,4 +1,94 @@
 class item {
-  constructor(Name, Type, Strength, Heal, Description) {}
-  let;
+  constructor(Name, Type, Strength, Heal, Description) {
+    this.Name = Name;
+    this.Type = Type;
+    this.Strength = Strength;
+    this.Heal = Heal;
+    this.Description = Description;
+  }
 }
+
+const firewood = item(
+  "Firewood",
+  "Weapon",
+  2,
+  null,
+  "An ordinary piece of wood. It makes you feel all fired up and warm on the inside. If you gather enough you could maybe make a campfire!"
+);
+const WeatheredSword = item(
+  "Weathered Sword",
+  "Weapon",
+  4,
+  null,
+  "A sword that has seen better days. It is rusty and dull, but it can still do some damage."
+);
+const bananaRang = item(
+  "Banana-Rang",
+  "Weapon",
+  3,
+  null,
+  "It's a banan'a, it's a bomeran'g, it's a Banan'rang! Although they dont come back when you throw them..."
+);
+const FarmorsDunderhonung = item(
+  "Farmors Dunderhonung",
+  "Potion",
+  6,
+  null,
+  "You dont know how she did it but farmor added dunder to the honung."
+);
+const ChugJug = item(
+  "Chug Jug",
+  "SuperHealing",
+  null,
+  100,
+  "Somehow this item travelled here from another world. It takes an entire 15 seconds to fully consume."
+);
+const PeculiarApple = item(
+  "Peculiar Apple",
+  "healing",
+  null,
+  10,
+  "Did you know? Gods of death love apples."
+);
+const Cheese = item(
+  "Cheese",
+  "healing",
+  null,
+  10,
+  "Know that you may one day eat this cheese fills you with determination."
+);
+const Crusefix = item(
+  "Crusefix",
+  "potion",
+  2,
+  10,
+  "A blessed crusefic from the priest pucci himself. As he once said: 'I'm going to obtain Heaven! Celebrate it... waht was born here is Heaven itself.'"
+);
+const escapeRope = item(
+  "Escape Rope",
+  "Weapon",
+  4,
+  null,
+  "'Run away? Who is running? Running where? Why would i run?!' Wip your opponent for even thinking you would run. 'Playtime is now over.'"
+);
+const Zanpakuto = item(
+  "Zanpakuto",
+  "Weapon",
+  6,
+  null,
+  "With enough training you can learn to use this sword to its full potential. But for now it is just a sword. 'You are not ready to achieve Bankai.'"
+);
+const DragonBall = item(
+  "Dragon Ball",
+  "Weapon",
+  1,
+  null,
+  "Collect all 7 and you can summon Shenron to grant you a wish. But be careful, he might not be as nice as you think. All in all it doesn't do much."
+);
+const flintEnSteel = item(
+  "Flint'n steel",
+  "Weapon",
+  2,
+  null,
+  "FLINT AND STEEL!"
+);
